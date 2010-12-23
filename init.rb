d5205311e46f032fe1d8638956a5f51d72acc9ca
Laas Toom <laas.toom@gmail.com>
2010-12-23 10:25:07 +0200

    Rails 3 style root and env access

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ require File.dirname(__FILE__) + '/lib/app_config'
 
 c = AppConfig.new
 ["config.yml","config.local.yml"].each do|relative_config_filename|
-  absolute_config_filename = File.join(RAILS_ROOT,'config',relative_config_filename)
+  absolute_config_filename = File.join(Rails.root,'config',relative_config_filename)
   c.use_file!(absolute_config_filename)
 end
-c.use_section!(RAILS_ENV)
+c.use_section!(Rails.env)
 ::Conf = c
